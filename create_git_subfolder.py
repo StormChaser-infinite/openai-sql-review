1d886289e28715d93d198c36ea5116b9fca3b0d5
StormChaser-infinite <105186977+StormChaser-infinite@users.noreply.github.com>
2023-12-04 14:47:29 +1300

    Added 'LISU' subfolder

--- create_git_subfolder.py
+++ create_git_subfolder.py
@@ -1,5 +1,6 @@
 from git import Repo 
 import os
+from azure.storage.blob import BlobServiceClient
 
 def create_subfolders(subfoldername: str, filename: str):
     """Create a subfolder with users' initial or email address when uploading the sql text file"""
@@ -34,14 +35,34 @@ def create_subfolders(subfoldername: str, filename: str):
         origin = repo.remote(name='origin')
         origin.push()
         print(f"Commit succeeded with the file uploaded: {subfoldername}, {filename} ")
+        write_blob(filename, subfoldername)
 
     except Exception as e:
         print(f"Create subfolder {subfoldername} and upload the file {filename} failed because {e}")
 
+def write_blob( subfolder: str, filename: str):
+    "Write the SQL Scripts from repo to storage account"
+    try: 
+        with open(filename, "r") as file:
+            file_content = file.read()
+            file.close()
+        blobservice = BlobServiceClient.from_connection_string(conn_str=  
+                                                               "DefaultEndpointsProtocol=https;AccountName=kittsqlmodel;AccountKey=6n/WfghjW+2xAN2h1iCiYxELJPADDC9h5Fr+iLbg+/1kBSoD8eVSeyeStKEyALWyNRE4XEBT8OJY+ASt5EspHQ==;EndpointSuffix=core.windows.net" )
+        file_name = subfolder+ "/"+ filename
+        container = blobservice.get_container_client("sql-inputs")
+        blob_client = container.get_blob_client(file_name)
+    
+        blob_client.upload_blob(file_content,  overwrite=True)
+
+        print(f"Successfully write the file - {filename} into {subfolder} blob container!")
+        
+    except Exception as e:
+        print(f"Fails to connect to the blob container, the error is {e}.")
+
 def main():
-    subfolder_name = "lilisun1"
+    subfolder_name = "lilisun"
     sql_input_name = "Test.txt"
     create_subfolders(subfolder_name, sql_input_name)
-
+    write_blob(subfolder_name, sql_input_name)
 main()
 
