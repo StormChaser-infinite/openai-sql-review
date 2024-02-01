3bc28206aa7061258b3b8eb1c3f9ee7dca4c44c6
StormChaser-infinite <105186977+StormChaser-infinite@users.noreply.github.com>
2024-02-02 11:28:21 +1300

    created engagement table in DB

---
+++ create_engagement_info.sql
@@ -0,0 +1,14 @@
+use kitt_sql_review;
+
+create table engagement_info (
+ID int,
+client_name varchar(255),
+engagement_code varchar(255),
+creater varchar(50),
+master_subquery varchar(10),
+versions int,
+file_names varchar(50),
+reviewers varchar(255),
+uploadtime datetime
+
+);
